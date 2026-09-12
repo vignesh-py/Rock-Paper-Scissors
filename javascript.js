0920c15250349ff682f1ce2e00f4ca01614d36e0
Vignesh-py <217954572+vignesh-py@users.noreply.github.com>
2026-09-12 14:29:45 +0530

    add main logical part

--- javascript.js
+++ javascript.js
@@ -9,16 +9,59 @@ function getComputerChoice() {  /*This function returns "Rock" "Paper" or "Sciss
    return "Rock";
   }
   else if (num ==1){
-   return "paper";
+   return "Paper";
   }
   else {
    return "Scissors";
   }
 }
 
- 
 function getHumanChoice(){
   choice=prompt("Enter your option");
   return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    Choice=humanChoice+computerChoice;
+    console.log(Choice)
+      if (Choice ==="RockRock"){
+          alert("Its a tie");
+      }
+      else if(Choice ==="ScissorsRock"){
+          alert("You loose Rock beats Scissors")
+      }
+      else if(Choice ==="PaperRock"){
+          alert("You Win Paper beats Rock")
+      }
+      else if(Choice ==="RockScissors"){
+          alert("You win Rock beats Scissors")
+      }
+      else if(Choice ==="ScissorsScissors"){
+          alert("Its a tie")
+      }
+      else if(Choice ==="PaperScissors"){
+          alert("You loose Scissors beats Paper")
+      }
+      else if(Choice ==="RockPaper"){
+          alert("You loose Paper beats Rock")
+      }
+      else if(Choice ==="ScissorsPaper"){
+          alert("You win Scissors beats Paper")
+      }
+      else if(Choice ==="PaperPaper"){
+          alert("Its a tie ")
+      }
+      else {
+          alert("Something went Wrong")
+      }
+          }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+
+
 
